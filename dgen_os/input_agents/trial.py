7a22adc518e894b6198f949bd531474f94158d0a
ljmaqele <41549613+ljmaqele@users.noreply.github.com>
2023-02-27 21:30:55 -0700

    adding test agents

---
+++ dgen_os/input_agents/trial.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 17 10:52:24 2022
+
+@author: lmaqelep
+"""
+
+import pandas as pd
+
+data = pd.read_pickle('agents_sfs_res_baseline.pkl')
+
